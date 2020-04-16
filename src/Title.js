af18de15a1c1fb2dfc48e18da23d22c685a6877c
Loreto Barroso <60405184+loretobarroso322@users.noreply.github.com>
2020-04-16 12:05:14 -0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,16 @@
+import './Title.css';
+// src/Title.js
+
+import React from 'react'
+
+function Title() {
+  return (
+    <div className="Title">
+    <header>
+      <h1>SFPOPOS</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Title
