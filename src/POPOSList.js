0902efdbf7d4d1818e3f44aecd1f1522272eb5a9
Loreto Barroso <60405184+loretobarroso322@users.noreply.github.com>
2020-04-19 21:46:30 -0700

    header component built

---
+++ src/POPOSList.js
@@ -0,0 +1,17 @@
+// src/POPOSList.js
+
+import React from 'react';
+import POPOSSpace from './POPOSSpace';
+
+function POPOSList() {
+  return (
+    <div>
+      <POPOSSpace />
+      <POPOSSpace />
+      <POPOSSpace />
+      <POPOSSpace />
+    </div>
+  )
+}
+
+export default POPOSList
