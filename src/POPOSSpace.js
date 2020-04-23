fb384855f16b7dbe001007431afc3cde19d715f8
Loreto Barroso <60405184+loretobarroso322@users.noreply.github.com>
2020-04-23 11:56:00 -0700

    dynamic content and footer

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -7,7 +7,7 @@ function POPOSSpace(props) {
   const { name, image, address } = props
   return (
     <div>
-      <img src= {`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300"/>>
+      <img src= {`${process.env.PUBLIC_URL}/images/${image}`} width="300" height="300"/>>
       <h1>{name}</h1>
      <div>{address}</div>
     </div>
